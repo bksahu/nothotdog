1baffe9e92777f869381834d718d58768b6e8965
bksahu <bablusahoo16@gmail.com>
2019-10-27 22:55:45 +0530

    Initail commit

---
+++ assets/main.js
@@ -0,0 +1,172 @@
+/*
+variables
+*/
+var model;
+var canvas;
+var classNames = [];
+var canvas;
+var coords = [];
+var mousePressed = false;
+var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+var height = (window.innerHeight > 0) ? window.innerHeight : screen.height;
+
+/*
+prepare the drawing canvas 
+*/
+$(function() {
+    canvas = window._canvas = new fabric.Canvas('canvas');
+    canvas.backgroundColor = '#ffffff';
+    canvas.isDrawingMode = 0;
+    canvas.freeDrawingBrush.color = "black";
+    canvas.freeDrawingBrush.width = 7;
+    canvas.renderAll();
+    //setup listeners 
+    canvas.on('mouse:up', function(e) {
+        getFrame();
+        mousePressed = false
+    });
+    canvas.on('mouse:down', function(e) {
+        mousePressed = true
+    });
+    canvas.on('mouse:move', function(e) {
+        recordCoor(e)
+    });
+})
+
+/*
+record the current drawing coordinates
+*/
+function recordCoor(event) {
+    var pointer = canvas.getPointer(event.e);
+    var posX = pointer.x;
+    var posY = pointer.y;
+
+    if (posX >= 0 && posY >= 0 && mousePressed) {
+        coords.push(pointer)
+    }
+}
+
+/*
+get the best bounding box by trimming around the drawing
+*/
+function getMinBox() {
+    //get coordinates 
+    var coorX = coords.map(function(p) {
+        return p.x
+    });
+    var coorY = coords.map(function(p) {
+        return p.y
+    });
+
+    //find top left and bottom right corners 
+    var min_coords = {
+        x: Math.min.apply(null, coorX),
+        y: Math.min.apply(null, coorY)
+    }
+    var max_coords = {
+        x: Math.max.apply(null, coorX),
+        y: Math.max.apply(null, coorY)
+    }
+
+    //return as strucut 
+    return {
+        min: min_coords,
+        max: max_coords
+    }
+}
+
+/*
+get the current image data 
+*/
+function getImageData() {
+    //get the minimum bounding box around the drawing 
+    const mbb = getMinBox()
+
+    //get image data according to dpi 
+    const dpi = window.devicePixelRatio
+    const imgData = canvas.contextContainer.getImageData(mbb.min.x * dpi, mbb.min.y * dpi,
+                                                  (mbb.max.x - mbb.min.x) * dpi, (mbb.max.y - mbb.min.y) * dpi);
+    return imgData
+}
+
+/*
+allow drawing on canvas
+*/
+function allowDrawing() {
+    canvas.isDrawingMode = 1;
+    document.getElementById('status').innerHTML = 'Model Loaded';
+    $('button').prop('disabled', false);
+}
+
+/*
+clear the canvs 
+*/
+function erase() {
+    canvas.clear();
+    canvas.backgroundColor = '#ffffff';
+    coords = [];
+}
+
+/*
+preprocess the data
+*/
+function preprocess(imgData) {
+    return tf.tidy(() => {
+        //convert to a tensor 
+        let tensor = tf.browser.fromPixels(imgData, numChannels = 1)
+        
+        //resize 
+        const resized = tf.image.resizeBilinear(tensor, [28, 28]).toFloat()
+        
+        //normalize 
+        const offset = tf.scalar(255.0);
+        const normalized = tf.scalar(1.0).sub(resized.div(offset));
+
+        //We add a dimension to get a batch shape 
+        const batched = normalized.expandDims(0)
+        return batched
+    })
+}
+
+/*
+load the model
+*/
+async function start() {
+    console.log("Loading the Model")
+    //load the model 
+    if (location.hostname === "localhost" || location.hostname === "127.0.0.1") {
+        model = await tf.loadLayersModel('http://127.0.0.1:5500/model3/model.json')
+    } else {
+        model = await tf.loadLayersModel('../model3/model.json')
+    }
+    
+    //warm up 
+    model.predict(tf.zeros([1, 28, 28, 1]))
+    
+    //allow drawing on the canvas 
+    allowDrawing()
+}
+
+/*
+get the prediction 
+*/
+function getFrame() {
+    //make sure we have at least two recorded coordinates 
+    if (coords.length >= 2) {
+
+        //get the image data from the canvas 
+        const imgData = getImageData()
+
+        //get the prediction 
+        const pred = model.predict(preprocess(imgData)).dataSync()
+        console.log(pred);
+        if (pred[0] <= 0.35) {
+            document.getElementById('status').innerHTML = 'That\'s a hotdog. 🤯';
+        } else if (pred[0] > 0.35) {
+            document.getElementById('status').innerHTML = 'That\'s not a hotdog. 🤨';
+        } else {
+            document.getElementById('status').innerHTML = 'Something went wrong. 😟';
+        }
+        
+    }
+}
